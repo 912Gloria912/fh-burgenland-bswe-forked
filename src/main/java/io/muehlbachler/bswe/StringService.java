2110dc4ce12c631b0e2a01b98e65466648bf6921
912Gloria912 <2310859022@fh-burgenland.at>
2024-10-09 19:34:06 +0200

    docs: added javaDocs to class StringService

--- src/main/java/io/muehlbachler/bswe/StringService.java
+++ src/main/java/io/muehlbachler/bswe/StringService.java
@@ -3,9 +3,11 @@ package io.muehlbachler.bswe;
 import org.springframework.stereotype.Service;
 
 // FIXME: add javadoc
+/** Class for StringService - Test*/
 @Service
 public class StringService {
     // FIXME: add javadoc
+/**The method return a String*/
     public String toUpperCase(final String string) {
         // FIXME: implement
         return string;
